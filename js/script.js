5df265b555fc8204c69a01d13d55fc369e8fac69
Alexander <black_star121@mail.ru>
2021-07-04 18:36:26 +0300

    update files

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function() {
+    $('.filter_group').click(function() {
+        $(this).toggleClass('active');
+    });
+
+    $('.img_menu, .menu_close .cross').click(function() {
+        $('.header_menu').toggleClass('active');
+    });
+
+    $('.pag_page_numb').click(function() {
+        $('.pag_page_numb').removeClass('active');
+        $(this).toggleClass('active');
+    });
+})
